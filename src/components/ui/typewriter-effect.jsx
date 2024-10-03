19aac716ee2491d54a6561abe61492700bb58cbc
Ronak Gupta <93486131+ronakgupta11@users.noreply.github.com>
2024-10-03 11:30:16 +0530

    fis typewriter

--- src/components/ui/typewriter-effect.jsx
+++ src/components/ui/typewriter-effect.jsx
@@ -12,7 +12,7 @@ export const TypewriterEffect = ({
   const wordsArray = words.map((word) => {
     return {
       ...word,
-      text: word.text.split(" "),
+      text: word.text.split(""),
     };
   });
 
