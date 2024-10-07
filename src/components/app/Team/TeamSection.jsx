0224cc7a6d59e3d540fae7f655ee53e2da28e9db
Ronak Gupta <93486131+ronakgupta11@users.noreply.github.com>
2024-10-07 12:02:21 +0530

    TEMA PAGE

--- src/components/app/Team/TeamSection.jsx
+++ src/components/app/Team/TeamSection.jsx
@@ -2,6 +2,9 @@
 import React from "react";
 import { SparklesCore } from "../../ui/sparkles";
 import { TeamCard } from "./TeamCard";
+import ronak from "../../../images/ronak_img.jpg"
+import shivam from "../../../images/shivam_img.jpeg"
+import bhavit from "../../../images/bhavit_img.jpeg"
 
 export default function SparklesPreview() {
   return (
@@ -36,9 +39,9 @@ export default function SparklesPreview() {
           className="absolute inset-0 w-full h-full bg-black [mask-image:radial-gradient(350px_200px_at_top,transparent_20%,white)]"></div>
       </div>
       <div className="flex flex-wrap items-center justify-around w-full">
-<TeamCard name={"Ronak Gupta"} image={""} desc={"SDE @ JPMorgan Chase"} twitter={"https://x.com/ronakguptarok07"} github={"https://github.com/ronakgupta11"}/>
-<TeamCard name={"Shivam Shaw"} desc={"SDE @ BNY Melon"} twitter={"https://x.com/ronakguptarok07"} github={"https://github.com/ronakgupta11"}/>
-<TeamCard name={"Bhavit Karnatak"} desc={"SDE @ Deutsche Bank"} twitter={"https://x.com/ronakguptarok07"} github={"https://github.com/ronakgupta11"}/>
+<TeamCard name={"Ronak Gupta"} image={ronak} desc={"SDE @ JPMorgan Chase"} twitter={"https://x.com/ronakguptarok07"} github={"https://github.com/ronakgupta11"}/>
+<TeamCard name={"Shivam Shaw"} image={shivam} desc={"SDE @ BNY Melon"} twitter={"https://x.com/tryptophan36"} github={"https://github.com/tryptophan36"}/>
+<TeamCard name={"Bhavit Karnatak"} image={bhavit} desc={"SDE @ Deutsche Bank"} twitter={"https://x.com/bhavitkarnatak"} github={"https://github.com/bhavit03"}/>
 
       </div>
     </div>)
